3bd2bcc6a4fec97526916740b4ecb928eb6d44b1
unknown <64092134+jeff922@users.noreply.github.com>
2022-10-17 08:18:55 -0400

    Add project files.

---
+++ MenuTest/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace MauiApp1.ViewModel;
+
+public partial class MainPageViewModel
+{
+    [RelayCommand]
+    public static void DoSomething()
+    {
+        // This point is never hit without workaround
+        return;
+    }
+}
